b52d74e0aa3ea707d6db8ed9df21599a2c08b6c9
notcimon <78378417+notcimon@users.noreply.github.com>
2021-09-27 18:18:03 +0200

    Update mensch_two_player.py

--- Aufgabe 4/Practice/mensch_two_player.py
+++ Aufgabe 4/Practice/mensch_two_player.py
@@ -2,21 +2,44 @@ import random
 import time
 
 #Setze Start-Werte für alle Figuren auf '0'.
-g1_og = 0, g1 = 0
-g2_og = 0, g2 = 0
-g3_og = 0, g3 = 0
-g4_og = 0, g4 = 0
 
-b1_og = 0, b1 = 0
-b2_og = 0, b2 = 0
-b3_og = 0, b3 = 0
-b4_og = 0, b4 = 0
 
 
 #WUERFELWURF [Funktion]
 def wuerfelwurf():
+    eyes = 0
     eyes = random.randint(1,6)
-    if turn == green:
+    if eyes == 6:
+        Sechs = True
+        return Sechs
+    elif eyes != 6:
+        Sechs = False
+        return Sechs
+    if Erste_Runde == True:
+        g1 = 0
+        g2 = 0
+        g3 = 0
+        g4 = 0
+
+        g1_og = 0
+        g2_og = 0
+        g3_og = 0
+        g4_og = 0
+
+
+        b1 = 0
+        b2 = 0
+        b3 = 0
+        b4 = 0
+
+        b1_og = 0
+        b2_og = 0
+        b3_og = 0
+        b4_og = 0
+    elif Erste_Runde == False:
+        pass
+
+    if turn == "green":
         if g1_og > g2_og and g1_og > g3_og and g1_og > g4_og and g1_og != 0 and g2_og != 0 and g3_og != 0 and g4_og != 0 and g1_og != 1 and g2_og != 1 and g3_og != 1 and g4_og != 1:
             g1 = g1_og + eyes
             g1_og = g1
@@ -29,7 +52,7 @@ def wuerfelwurf():
         elif g4_og > g1_og and g4_og > g2_og and g4_og > g3_og and g1_og != 0 and g2_og != 0 and g3_og != 0 and g4_og != 0 and g1_og != 1 and g2_og != 1 and g3_og != 1 and g4_og != 1:
             g4 = g4_og + eyes
             g4_og = g4
-    elif turn == blue:
+    elif turn == "black":
         if b1_og > b2_og and b1_og > b3_og and b1_og > b4_og:
             b1 = b1_og + eyes
             b1_og = b1
@@ -46,6 +69,29 @@ def wuerfelwurf():
 
 #Figuren SCHLAGEN [Funktion]
 def figuren_schlagen():
+    if Erste_Runde == True:
+        g1 = 0
+        g2 = 0
+        g3 = 0
+        g4 = 0
+
+        g1_og = 0
+        g2_og = 0
+        g3_og = 0
+        g4_og = 0
+
+
+        b1 = 0
+        b2 = 0
+        b3 = 0
+        b4 = 0
+
+        b1_og = 0
+        b2_og = 0
+        b3_og = 0
+        b4_og = 0
+    elif Erste_Runde == False:
+        pass
     if g1 == b1 or g1 == b2 or g1 == b3 or g1 == b4:
         #Fuer g1:
         if g1 == b1 - 20 or b1 == g1 - 20 and b1 != 41 and b1 != 42 and b1 != 43 and b1 != 44:
@@ -83,17 +129,46 @@ def figuren_schlagen():
             b3 = 0
         elif g4 == b4 - 20 or b4 == g4 - 20 and b4 != 41 and b4 != 42 and b4 != 43 and b4 != 44:
             b4 = 0
-    return g1, g2, g3, g4, b1, b2, b3, b4 
+    return g1, g2, g3, g4, b1, b2, b3, b4, g1_og, g2_og, g3_og, g4_og, b1_og, b2_og, b3_og, b4_og
 
 
 #Abfolge bei Zug
+GameOver = False
+Erste_Runde = True
 while GameOver == False:
     
     #g an der Reihe:
-    eyes = 0
+    turn = "green"
     wuerfelwurf()
     figuren_schlagen()
-    while eyes == 6:
+    if Sechs == True:
         wuerfelwurf()
         figuren_schlagen()
     print('g1: ', g1, 'g2:', g2, 'g3:', g3, 'g4:', g4)
+    print('b1: ', b1, 'b2:', b2, 'b3:', b3, 'b4:', b4)
+    #b an der Reihe:
+    turn = "black"
+    wuerfelwurf()
+    figuren_schlagen()
+    while Sechs == True:
+        wuerfelwurf()
+        figuren_schlagen()
+    print('g1: ', g1, 'g2:', g2, 'g3:', g3, 'g4:', g4)
+    print('b1: ', b1, 'b2:', b2, 'b3:', b3, 'b4:', b4)
+
+
+    #'GAMEOVER-CHECKER'
+    if g1 >= 41 and g2 >= 41 and g3 >= 41 and g4 >= 41:
+        GameOver = True
+        print('GRUEN hat gewonnen!!!')
+    elif b1 >= 41 and b2 >= 41 and b3 >= 41 and b4 >= 41:
+        GameOver = True
+        print('SCHWARZ hat gewonnen!!!')
+    else:
+        continue
+    
+    #'Erste_Runde Index'
+    if Erste_Runde == True:
+        Erste_Runde = False
+    elif Erste_Runde == False:
+        pass
